f5bb66a75aa8e3c68bb76ed87ff332ab9c42a811
Dan Abramov <dan.abramov@gmail.com>
2023-05-09 00:14:32 +0100

    rm

--- fixtures/flight/src/ShowMore.js
+++ fixtures/flight/src/ShowMore.js
@@ -2,12 +2,9 @@
 
 import * as React from 'react';
 
-import Container from './Container.js';
-
 export default function ShowMore({children}) {
   const [show, setShow] = React.useState(false);
   if (!show) {
     return <button onClick={() => setShow(true)}>Show More</button>;
   }
-  return <Container>{children}</Container>;
 }
