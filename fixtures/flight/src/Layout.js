af50b459d6b73e2cea822013bd7492611c135ed4
Dan Abramov <dan.abramov@gmail.com>
2023-05-10 05:57:22 -0600

    blah

--- fixtures/flight/src/Layout.js
+++ fixtures/flight/src/Layout.js
@@ -3,7 +3,10 @@ import * as React from 'react'
 export default function Layout({ children }) {
   return (
     <html>
-      <body>
+      <body style={{
+        backgroundColor: 'rgb(41, 44, 52)',
+        transition: 'background-image 1s ease-in-out',
+      }}>
         <link rel="stylesheet" href="/main.css" />
         <div className="container">
           <div className="background" />
@@ -13,3 +16,44 @@ export default function Layout({ children }) {
     </html>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+'use client';
+
+import * as React from 'react'
+import { useState } from 'react'
+import { ThemeContext } from './Theme.js';
+
+export default function Layout({ children }) {
+  const [ pageTheme, setPageTheme] = useState(null);
+  return (
+    <html>
+      <body style={{
+        backgroundColor: 'rgb(41, 44, 52)',
+        backgroundImage: pageTheme,
+        transition: 'background-image 1s ease-in-out',
+      }}>
+        <link rel="stylesheet" href="/main.css" />
+        <div className="container">
+          <div className="background" />
+          <ThemeContext.Provider value={setPageTheme}>
+            {children}
+          </ThemeContext.Provider>
+        </div>
+      </body>
+    </html>
+  );
+}
+*/
--- fixtures/flight/src/Post.js
+++ fixtures/flight/src/Post.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { readFile } from 'fs/promises';
 import Markdown from 'react-markdown';
+import {ThemeButton} from './Theme.js';
 
 export default async function Post({ slug, isExcerpt, children }) {
   let markdown = await readFile('./posts/' + slug + '.md', 'utf8');
@@ -15,3 +16,40 @@ export default async function Post({ slug, isExcerpt, children }) {
     </Markdown>
   );
 }
+
+
+
+
+
+
+
+
+
+
+import imageToGradient from 'image-to-gradient';
+
+async function Image(props) {
+  const gradient = await getGradient('./public' + props.src);
+  return (
+    <ThemeButton theme={gradient}>
+      <img {...props} />
+    </ThemeButton>
+  );
+}
+
+async function getGradient(src) {
+  return new Promise((resolve, reject) => {
+    imageToGradient(src, {
+      angle: 10, // gradient angle in degrees
+      steps: 64  // number of steps
+    }, function(err, cssGradient) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(cssGradient);
+      }
+    });
+  })  
+}
+
+
