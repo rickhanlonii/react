5a0b7111713f92fa2bf58e1e10a21eae5a83238f
Rick Hanlon <rickhanlonii@fb.com>
2024-06-01 14:42:35 -0400

    change artifacts

--- scripts/actions/download_artifacts.js
+++ scripts/actions/download_artifacts.js
@@ -16,7 +16,7 @@ function execHelper(command, options, streamStdout = false) {
 }
 
 async function run({github, context, core, fetch}) {
-  let artifactsUrl = `https://circleci.com/api/v1.1/project/github/facebook/react/891966/artifacts`;
+  let artifactsUrl = `https://circleci.com/api/v1.1/project/github/facebook/react/892222/artifacts`;
   // This is a temporary, dirty hack to avoid needing a GitHub auth token in the circleci
   // workflow to notify this GitHub action. Sorry!
   if (!artifactsUrl) {
