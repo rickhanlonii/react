9f240ac5851a9fc4387b89303021db6372f1313b
Rick Hanlon <rickhanlonii@meta.com>
2026-02-11 16:53:09 -0500

    feat: add Express RSC server with react-server-dom-webpack

--- server/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>react-dom-native</h1>
-      <p>This RSC page will be rendered natively on iOS.</p>
-    </div>
-  );
-}
---
+++ server/src/App.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var Counter = require('../../example/components/Counter');
+var TextInput = require('../../example/components/TextInput');
+
+function App() {
+  var timestamp = new Date().toLocaleTimeString();
+
+  return (
+    <div style={{padding: 20, gap: 16}}>
+      <h1>react-dom-native</h1>
+      <p>This page is rendered by React Server Components on native iOS.</p>
+
+      <div style={{gap: 12}}>
+        <h2>Counter</h2>
+        <Counter initialCount={0} />
+      </div>
+
+      <div style={{gap: 12}}>
+        <h2>Text Input</h2>
+        <TextInput placeholder="Say hello..." />
+      </div>
+
+      <p style={{color: '#888', fontSize: 12}}>
+        {'Rendered at ' + timestamp}
+      </p>
+    </div>
+  );
+}
+
+module.exports = App;
+module.exports.default = App;
