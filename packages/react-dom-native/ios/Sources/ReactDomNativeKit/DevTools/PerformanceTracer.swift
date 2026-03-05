5980392f34e8c5b245b877abd08e516506c454dd
Rick Hanlon <rickhanlonii@meta.com>
2026-03-05 16:08:02 -0500

    change paint trace colors to tertiary (green)

--- packages/react-dom-native/ios/Sources/ReactDomNativeKit/DevTools/PerformanceTracer.swift
+++ packages/react-dom-native/ios/Sources/ReactDomNativeKit/DevTools/PerformanceTracer.swift
@@ -358,7 +358,7 @@ class PerformanceTracer {
         let preparePaintEnd = (t["preparePaintEnd"] as? Double) ?? 0
         if preparePaintEnd > preparePaintStart {
             reportTimeStamp(label: "Prepare Paint", start: preparePaintStart, end: preparePaintEnd,
-                track: "Shadow Tree", trackGroup: "Native ⚛", color: "secondary")
+                track: "Shadow Tree", trackGroup: "Native ⚛", color: "tertiary")
         }
 
         let screenshotStart = (t["screenshotStart"] as? Double) ?? 0
